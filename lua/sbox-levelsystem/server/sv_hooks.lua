79c8424a81dd578fd941e8e2f9141c03f966b041
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-07-20 14:34:22 -0400

    Update sv_hooks.lua

--- lua/sbox-levelsystem/server/sv_hooks.lua
+++ lua/sbox-levelsystem/server/sv_hooks.lua
@@ -1,7 +1,7 @@
 local function checkPlayerDatabase(ply)
 	local data = sql.Query("SELECT * FROM sbox_levelsystem WHERE player = " .. ply:SteamID64() .. ";")
 	if not data then
-        sql.Query("INSERT INTO sbox_levelsystem (player, plyname) VALUES (" .. ply:SteamID64() .. ", " .. ply:Name() .. ");")
+        sql.Query("INSERT INTO sbox_levelsystem (player, plyname, level, xp) VALUES (" .. ply:SteamID64() .. ", " .. ply:Name() .. ", 1, 0);")
     end
 end
 
