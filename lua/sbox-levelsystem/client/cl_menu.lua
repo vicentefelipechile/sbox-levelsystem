b3fe5e9e80294191cbccfd83c320cc8f6c47395d
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-07-20 22:56:54 -0400

    commit

--- lua/sbox-levelsystem/client/cl_menu.lua
+++ lua/sbox-levelsystem/client/cl_menu.lua
@@ -19,27 +19,47 @@ net.Receive("sbox_levelsystem_net", function()
     ------------- Labels -------------
     ----------------------------------
     local s1Label = vgui.Create("DLabel", sFrame)
-    s1Label:SetPos(5, 30)
+    s1Label:SetPos(10, 30)
     s1Label:SetSize(285, 16)
     s1Label:SetText(SLS_GetLanguage("level_current") .. ": " .. level)
 
+    local s1Icon = vgui.Create("DImage", sFrame)
+    s2Icon:SetPos(5, 30)
+    s2Icon:SetSize(16, 16)
+    s2Icon:SetImage("sbox_levelsystem/level_add.png")
+
     local s2Label = vgui.Create("DLabel", sFrame)
-    s2Label:SetPos(5, 50)
+    s2Label:SetPos(10, 50)
     s2Label:SetSize(285, 16)
     s2Label:SetText(SLS_GetLanguage("level_next1") ..  xp_total - xp .. " " .. SLS_GetLanguage("xp") .. SLS_GetLanguage("level_next2"))
 
+    local s2Icon = vgui.Create("DImage", sFrame)
+    s2Icon:SetPos(5, 50)
+    s2Icon:SetSize(16, 16)
+    s2Icon:SetImage("sbox_levelsystem/xp_add.png")
+
     local s3Label = vgui.Create("DLabel", sFrame)
-    s3Label:SetPos(5, 70)
+    s3Label:SetPos(10, 70)
     s3Label:SetSize(285, 16)
     s3Label:SetText(SLS_GetLanguage("xp_current") .. ": " .. xp .. " / " .. xp_total)
 
+    local s3Icon = vgui.Create("DImage", sFrame)
+    s3Icon:SetPos(5, 70)
+    s3Icon:SetSize(16, 16)
+    s3Icon:SetImage("sbox_levelsystem/xp.png")
+
     local s4Label = vgui.Create("DLabel", sFrame)
-    s4Label:SetPos(5, 110)
+    s4Label:SetPos(10, 110)
     s4Label:SetSize(285, 16)
     s4Label:SetText(SLS_GetLanguage("level") .. ": " .. level)
 
+    local s4Icon = vgui.Create("DImage", sFrame)
+    s4Icon:SetPos(5, 110)
+    s4Icon:SetSize(16, 16)
+    s4Icon:SetImage("sbox_levelsystem/level.png")
+
     local s5Label = vgui.Create("DLabel", sFrame)
-    s5Label:SetPos(5, 120)
+    s5Label:SetPos(10, 120)
     s5Label:SetSize(285, 16)
     s5Label:SetText(SLS_GetLanguage("progress") .. ": " .. math.Round(xp / xp_total * 100) .. "%")
 
