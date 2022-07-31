bc6c8e842d1ae0e6e36a1dea950a39b0ba20bb0a
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-07-30 20:17:32 -0400

    Update sv_announce.lua

--- lua/sbox-levelsystem/server/sv_announce.lua
+++ lua/sbox-levelsystem/server/sv_announce.lua
@@ -1,7 +1,7 @@
 hook.Add("SLS_LevelUp", "LevelUpLololololol", function(ply, level)
     if sbox_ls.display_level then
         for _, v in ipairs(player.GetAll()) do
-            v:ChatPrint("[SBOX-LS] " .. ply:Nick() .. " " .. SLS_GetLanguage("levelup") .. " " .. level.. ".")
+            v:ChatPrint(SLS_GetLanguage("prefix") .. ply:Nick() .. " " .. SLS_GetLanguage("levelup") .. " " .. level.. ".")
         end
     end
 end)
