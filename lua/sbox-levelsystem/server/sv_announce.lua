b7fa44a68ae55135bee45349bc32ce47b14d5a87
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-08-04 13:32:58 -0400

    Update sv_announce.lua

--- lua/sbox-levelsystem/server/sv_announce.lua
+++ lua/sbox-levelsystem/server/sv_announce.lua
@@ -2,7 +2,7 @@ hook.Add("onPlayerLevelUp", "onPlayerLevelUp", function(ply, level)
 
     if sbox_ls.display_level then
 
-        net.Start("darkrp_levelsystem_levelup")
+        net.Start("sandbox_levelsystem_levelup")
             net.WriteTable({
                 lvl = tostring(level),
                 name = ply:Nick()
