948a21f59ebe3c6fb5e61a1839a94e8497f4bd3b
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-09-18 23:23:58 -0300

    Update levelsystem.lua

--- lua/ulx/modules/sh/levelsystem.lua
+++ lua/ulx/modules/sh/levelsystem.lua
@@ -10,3 +10,16 @@ addlevel:addParam{ type=ULib.cmds.PlayerArg }
 addlevel:addParam{ type=Ulib.cmds.NumArg, min=1, max=#sbox_ls["levels"]}
 addlevel:defaultAccess( ULib.ACCESS_SUPERADMIN )
 addlevel:help( "Add a level to an user" )
+
+
+
+function ulx.setlevel( calling_ply, target, number )
+    target:SetPlayerLevel(number)
+
+	ulx.fancyLogAdmin( calling_ply, "#A set level #s to #T", level, target )
+end
+local setlevel = ulx.command("Level System", "ulx setlevel", ulx.setlevel, "!setlevel")
+setlevel:addParam{ type=ULib.cmds.PlayerArg }
+setlevel:addParam{ type=Ulib.cmds.NumArg, min=1, max=#sbox_ls["levels"]}
+setlevel:defaultAccess( ULib.ACCESS_SUPERADMIN )
+setlevel:help( "Set a level to an user" )
