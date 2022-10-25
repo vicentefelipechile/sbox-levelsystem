80f5ada4ee87ea7c9e9ee93e15119cc7615d3f86
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-10-25 15:29:56 -0300

    commit

--- lua/sbox-levelsystem/client/cl_commands.lua
+++
@@ -1,8 +0,0 @@
-concommand.Add("sbox_ls_gmodstats", function(ply)
-    if sbox_ls.maths_enable and sql.TableExists(sbox_ls.math_db) then
-        SLS_ext_GetGmodStats(ply)
-        print(sbox_ls.prefix .. SLS.GetLanguage("updated") .. "\n")
-    else
-        print(sbox_ls.prefix .. SLS.GetLanguage("disabled_gmodstats") .. "\n")
-    end
-end)
