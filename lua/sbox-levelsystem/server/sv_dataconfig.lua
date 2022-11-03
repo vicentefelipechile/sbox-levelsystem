818e9ac341d3cb1a043a4c9f03055b4a3152240f
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-11-03 18:11:07 -0300

    Update sv_dataconfig.lua

--- lua/sbox-levelsystem/server/sv_dataconfig.lua
+++ lua/sbox-levelsystem/server/sv_dataconfig.lua
@@ -8,7 +8,9 @@ function SLS.requestData()
     http.Fetch("https://raw.githubusercontent.com/SuperCALIENTITO/sbox-levelsystem/main/data/sbox-levelsystem/config.txt",
         function(body, _, _, response)
             if response == 200 then
-                config = body
+                file.Write(sbox_ls.dir .. "/config.txt", body)
+                SLS.mSV("Data has been restored")
+                config = response
             else
                 SLS.mSV("ERROR: "..response)
                 SLS.mSV("FAILED TO FETCH, YOU CAN'T USE CONFIG")
@@ -37,19 +39,12 @@ function SLS.checkData()
 
     if not file.Exists(dir.."config.txt", "DATA") then
         file.CreateDir(sbox_ls.dir)
-
-        local data = SLS.requestData()
-
-        if data then
-            file.Write(dir.."config.txt", data)
-            file.Write(dir.."readme.txt", SLS.GetLanguage("readme"))
-        end
+        file.Write(dir.."config.txt", "")
+        file.Write(dir.."readme.txt", SLS.GetLanguage("readme"))
     end
 
     if file.Read(dir.."config.txt", "DATA") == "" then
-        local data = SLS.requestData()
-
-        file.Write(dir.."config.txt", data)
+        SLS.requestData()
     end
 end
 
